4a1eb5df417715cf5d4c52a5122f971460e93963
damien henry <dh7777777@gmail.com>
2024-01-20 17:13:28 +0100

    add default date when no Exif

--- update_image_filedate.py
+++ update_image_filedate.py
@@ -1,44 +1,70 @@
 # Update the file date of images to the EXIF date
-from PIL import Image
-import os
+# If no EXIF date is available, use the default date
+# If no default date is provided, the file date is not changed
+#
+# Usage: update_image_filedate.py <directory>
+#        update_image_filedate.py <directory> -d "2020-01-01 12:00:00"
+
+import argparse
 import datetime
 import glob
-import sys
+import os
+from PIL import Image
 
 
-def change_file_date_to_exif_date(file_path):
+def change_file_date_to_exif_date(file_path, default_date=None):
     try:
         with Image.open(file_path) as img:
             exif_data = img._getexif()
-            if exif_data is not None:
-                # 36867 is the tag for DateTimeOriginal
-                date_time_original = exif_data.get(36867)
+            new_date = None
+
+            if exif_data:
+                date_time_original = exif_data.get(
+                    36867
+                )  # 36867 is the tag for DateTimeOriginal
                 if date_time_original:
                     new_date = datetime.datetime.strptime(
                         date_time_original, "%Y:%m:%d %H:%M:%S"
                     )
-                    timestamp = new_date.timestamp()
-                    os.utime(file_path, (timestamp, timestamp))
-                    print(f"Date modified for '{file_path}' to {new_date}")
-                else:
-                    print(f"No original date found for '{file_path}'")
+
+            if new_date is None and default_date:
+                new_date = default_date
+
+            if new_date:
+                timestamp = new_date.timestamp()
+                os.utime(file_path, (timestamp, timestamp))
+                print(f"Date modified for '{file_path}' to {new_date}")
             else:
-                print(f"No EXIF data found for '{file_path}'")
+                print(f"No valid date for '{file_path}'. Skipping.")
     except Exception as e:
         print(f"Error processing '{file_path}': {e}")
 
 
-def process_folder(folder_path):
+def process_folder(folder_path, default_date=None):
     for file_path in glob.glob(os.path.join(folder_path, "*.jpg")) + glob.glob(
         os.path.join(folder_path, "*.jpeg")
     ):
-        change_file_date_to_exif_date(file_path)
+        change_file_date_to_exif_date(file_path, default_date)
+
+
+def parse_date(date_string):
+    try:
+        return datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        raise argparse.ArgumentTypeError("Date must be in YYYY-MM-DD HH:MM:SS format")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python update_image_dates.py <folder_path>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(
+        description="Update image file dates based on EXIF data or a default date."
+    )
+    parser.add_argument("folder", help="Path to the folder containing images")
+    parser.add_argument(
+        "-d",
+        "--default_date",
+        help="Default date in YYYY-MM-DD HH:MM:SS format",
+        type=parse_date,
+    )
+    args = parser.parse_args()
 
-    folder_path = sys.argv[1]
-    process_folder(folder_path)
+    process_folder(args.folder, args.default_date)
